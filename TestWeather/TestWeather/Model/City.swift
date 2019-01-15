618d203a56e044a74b1e604948082f3c21d7b128
user <kubaa771@gmail.com>
2019-01-15 10:42:23 +0100

    initial commit

---
+++ TestWeather/TestWeather/Model/City.swift
@@ -0,0 +1,18 @@
+//
+//  City.swift
+//  TestWeather
+//
+//  Created by user on 14/01/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import Foundation
+
+struct City {
+    let name: String
+    let id: Int
+    let image: String
+    let description: String
+    var isExpanded: Bool
+    
+}
---
+++ TestWeather/TestWeather/Model/Weather.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  TestWeather
+//
+//  Created by Jakub Iwaszek on 14/01/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import Foundation
+
+struct Weather {
+    var rain :Double
+    var sun :Int
+    var temperature :Int
+    var wind: Int
+    var storm: Bool
+}
