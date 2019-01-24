66fa7280356ba8252ace5078a1bf2636177a84f3
Jakub Iwaszek <kubaa771@gmail.com>
2019-01-24 16:32:17 +0100

    updating iphone 5 view

---
+++ ShopApp/ShopApp/ListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListTableViewCell.swift
+//  ShopApp
+//
+//  Created by Jakub Iwaszek on 24/01/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
